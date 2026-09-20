188ef70ac5618c90eb91cc657ac4c42301c52b42
ianhint <ismail4878199@gmail.com>
2026-09-20 10:24:32 +0600

    first commit

---
+++ 19sept2026/php_syntax.php
@@ -0,0 +1,26 @@
+<!-- General nsyntax -->
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="<?php ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php 
+echo "Hello Word"
+?>
+
+
+<!-- Short Open Tag syntax -->
+
+<? echo "Hello using Short Open tag" ?>
+
+<!--  Short circuit syntax -->
+
+<?= "Hello Using Short circuit" ?>
+    
+</body>
+</html>
---
+++ 19sept2026/var_dump.php
@@ -0,0 +1,15 @@
+<?php
+
+$teams = ["England","Australia", "Newzeland"];
+
+$numbers =100;
+$name = "Rifait bhuiyannn ";
+$money =1000.00;
+
+echo "<pre>";
+
+var_dump ($teams);
+var_dump ($numbers);
+var_dump ($money);
+
+?>
